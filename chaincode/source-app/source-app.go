a02b8753d421f8dbaa0b4df20c8dd1201b97bc30
h-hg <1394564544@qq.com>
2019-05-28 22:44:40 +0800

    add annotation of chaincode's attribute

--- chaincode/source-app/source-app.go
+++ chaincode/source-app/source-app.go
@@ -9,52 +9,47 @@ import (
     "github.com/hyperledger/fabric/core/chaincode/shim"
     pb "github.com/hyperledger/fabric/protos/peer"
 )
-type ExpressChainCode struct{	
+type ExpressChainCode struct{
 }
 
 //express数据结构体
 type ExpressInfo struct{
-    ExpressID string `json:ExpressID`                             //食品ID
-    ExpressProInfo ProInfo `json:ExpressProInfo`                  //生产信息
-    //ExpressIngInfo []IngInfo `json:ExpressIngInfo`                  //配料信息
+    ExpressID string `json:ExpressID`                             //快递ID
+    ExpressProInfo ProInfo `json:ExpressProInfo`                  //快递信息
     ExpressLogInfo LogInfo `json:ExpressLogInfo`                  //物流信息
 }
 
 type ExpressAllInfo struct{
     ExpressID string `json:ExpressId`
     ExpressProInfo ProInfo `json:ExpressProInfo`
-    //ExpressIngInfo []IngInfo `json:ExpressIngInfo`
     ExpressLogInfo []LogInfo `json:ExpressLogInfo`
 }
 
-//生产信息
+//快递信息
 type ProInfo struct{
-    CoName string `json:CoName`                         //食品名称
-    CoInfo string `json:CoInfo`                         //食品规格
-    DeliverTime string `json:DeliverTime`                   //食品出产日期
-    EstimatedDeliveryTime string `json:EstimatedDeliveryTime`                   //食品保质期
-    BatchNum string `json:BatchNum`                           //食品批次号
-    Weight string `json:Weight`                         //食品生产许可证编号
-    Price string `json:Price`                 //食品生产商名称
-    Deliverer string `json:Deliverer`                 //食品生产价格
-    DelivererAdd string `json:DelivererAdd`                 //食品生产所在地
+    CoName string `json:CoName`                              //物流公司名称
+    CoInfo string `json:CoInfo`                              //物流公司信息
+    DeliverTime string `json:DeliverTime`                    //发件日期
+    EstimatedDeliveryTime string `json:EstimatedDeliveryTime`//预计配送时间
+    BatchNum string `json:BatchNum`                          //快递批次号
+    Weight string `json:Weight`                              //快递重量
+    Price string `json:Price`                                //快递费用
+    Deliverer string `json:Deliverer`                        //投件人
+    DelivererAdd string `json:DelivererAdd`                  //投件人所在地
 }
-/* type IngInfo struct{
-    IngID string `json:IngID`                               //配料ID
-    IngName string `json:IngName`                           //配料名称
-} */
+
 
 type LogInfo struct{
-    ArrTime string `json:ArrTime`             //出发时间
-    TranferStationAdd string `json:TranferStationAdd`                 //到达时间
-    HandlerInfo string `json:HandlerInfo`                     //处理业务(储存or运输)
-    PkgStatus string `json:PkgStatus`             //出发地
-    DepartureTime string `json:DepartureTime`                           //目的地
-    NextDestAdd string `json:NextDestAdd`                   //销售商
-    Mission string `json:Mission`                 //存储时间
-    VehicleType string `json:VehicleType`                             //运送方式
-    VehicleInfo string `json:VehicleInfo`                     //物流公司名称
-    DriverInfo string `json:DriverInfo`                           //费用
+    ArrTime string `json:ArrTime`                       //到达时间
+    TranferStationAdd string `json:TranferStationAdd`   //中转站地址
+    HandlerInfo string `json:HandlerInfo`               //处理人员信息
+    PkgStatus string `json:PkgStatus`                   //快递包状态
+    DepartureTime string `json:DepartureTime`           //出发时间
+    NextDestAdd string `json:NextDestAdd`               //下一个站点地址
+    Mission string `json:Mission`                       //转运/派送
+    VehicleType string `json:VehicleType`               //运送方式
+    VehicleInfo string `json:VehicleInfo`               //交通工具信息
+    DriverInfo string `json:DriverInfo`                 //司机信息
 }
 
 func (a *ExpressChainCode) Init(stub shim.ChaincodeStubInterface) pb.Response {
@@ -115,38 +110,6 @@ func (a *ExpressChainCode) addProInfo(stub shim.ChaincodeStubInterface, args []s
     return shim.Success(nil)
 }
 
-/* func(a *ExpressChainCode) addIngInfo (stub shim.ChaincodeStubInterface,args []string) pb.Response{
-        
-    var ExpressInfos ExpressInfo
-    var IngInfoitem IngInfo
-
-    if  (len(args)-1)%2 != 0 || len(args) == 1{
-        return shim.Error("Incorrect number of arguments")
-    }
-
-    ExpressID := args[0]
-    for i :=1;i < len(args);{   
-        IngInfoitem.IngID = args[i]
-        IngInfoitem.IngName = args[i+1]
-        ExpressInfos.ExpressIngInfo = append(ExpressInfos.ExpressIngInfo,IngInfoitem)
-        i = i+2
-    }
-    
-    
-    ExpressInfos.ExpressID = ExpressID
-    //ExpressInfos.ExpressIngInfo = expressIngInfo
-    IngInfoJsonAsBytes,err := json.Marshal(ExpressInfos)
-    if err != nil {
-    return shim.Error(err.Error())
-    }
-
-    err = stub.PutState(ExpressInfos.ExpressID,IngInfoJsonAsBytes)
-    if err != nil{
-        return shim.Error(err.Error())
-    }
-    return shim.Success(nil)
-        
-} */
 
 func(a *ExpressChainCode) addLogInfo (stub shim.ChaincodeStubInterface,args []string) pb.Response{
  
@@ -254,38 +217,6 @@ func(a *ExpressChainCode) getProInfo (stub shim.ChaincodeStubInterface,args []st
     return shim.Success(jsonsAsBytes)
 }
 
-/* func(a *ExpressChainCode) getIngInfo (stub shim.ChaincodeStubInterface,args []string) pb.Response{
- 
-    if len(args) !=1{
-        return shim.Error("Incorrect number of arguments.")
-    }
-    ExpressID := args[0]
-    resultsIterator,err := stub.GetHistoryForKey(ExpressID)
-
-    if err != nil{
-        return shim.Error(err.Error())
-    }
-    defer resultsIterator.Close()
-    
-    var expressIngInfo []IngInfo
-    for resultsIterator.HasNext(){
-        var ExpressInfos ExpressInfo
-        response,err := resultsIterator.Next()
-        if err != nil{
-            return shim.Error(err.Error())
-        }
-        json.Unmarshal(response.Value,&ExpressInfos)
-        if ExpressInfos.ExpressIngInfo != nil{
-            expressIngInfo = ExpressInfos.ExpressIngInfo
-            continue
-        }
-    }
-    jsonsAsBytes,err := json.Marshal(expressIngInfo)
-    if err != nil{
-        return shim.Error(err.Error())
-    }
-    return shim.Success(jsonsAsBytes)
-} */
 
 func(a *ExpressChainCode) getLogInfo (stub shim.ChaincodeStubInterface,args []string) pb.Response{
 
