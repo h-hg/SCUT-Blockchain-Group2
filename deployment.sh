fd5c4d4417aecf13fa50aff2047452ed2b80d9d3
h-hg <1394564544@qq.com>
2019-05-28 21:16:25 +0800

    modify resource path of front-end

--- deployment.sh
+++ deployment.sh
@@ -6,14 +6,15 @@ docker rm -f $(docker ps -aq)
 # 删除注册的管理员和用户
 rm -f -r ./.hfc-key-store
 # 下载代码仓库
-rm -f -r ./SCUT-Blockchain-Group2
-git clone https://github.com/h-hg/SCUT-Blockchain-Group2.git
+# rm -f -r ./SCUT-Blockchain-Group2
+# git clone https://github.com/h-hg/SCUT-Blockchain-Group2.git
 # 设置权限
 cd ./SCUT-Blockchain-Group2/
 cd ./basic-network
-chmod a+x start.sh
+chmod a+x ./start.sh
 cd ../source-app
-chmod a+x startFabric.sh
+chmod a+x ./startFabric.sh
+./startFabric.sh
 # 安装包
 npm install
 # 更改端口
